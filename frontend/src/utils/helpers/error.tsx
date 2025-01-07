e4fd57dae5c09f082027a6274d3532e9f9982fb7
izzul-ali <izzuldev@gmail.com>
2025-01-07 08:44:22 +0700

    feat: adjust error handling

--- frontend/src/utils/helpers/error.tsx
+++ frontend/src/utils/helpers/error.tsx
@@ -2,17 +2,12 @@ import { IGlobalErrorResponse } from "@/interfaces/global.interface"
 import { isAxiosError } from "axios"
 import { signOut } from "next-auth/react"
 import { toast } from "react-toastify"
-import { isDirectusError } from "@directus/errors"
 import { isArray } from "lodash"
 
 /**
- * Handling global error
+ * Handling global errors from react-query
  */
 export async function onError(err: any) {
-  console.log("t-log-global-error", err)
-
-  err = JSON.parse(JSON.stringify(err))
-
   if (isAxiosError(err)) {
     const errData = err.response?.data as {
       errors: IGlobalErrorResponse[]
